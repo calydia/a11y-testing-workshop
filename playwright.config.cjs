4029adac229bdfcdd189db227c31dd82e497eae2
Kari Kramsi <kari.kramsi@gmail.com>
2024-11-24 20:05:27 +0200

    test: Add dedicated port for tests

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:4321',
+    baseURL: 'http://127.0.0.1:4322',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,8 +71,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm start',
-    url: 'http://127.0.0.1:4321',
+    command: 'npm run test:server',
+    url: 'http://127.0.0.1:4322',
     reuseExistingServer: !process.env.CI,
   },
 });
